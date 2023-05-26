6b1485e8bf89c0d11cf317df5a41ad8b49750726
ReneKubax <therene21@gmail.com>
2023-05-25 21:30:09 -0600

    Complete App

--- src/redux/features/counterSlice.ts
+++ src/redux/features/counterSlice.ts
@@ -1,22 +1,41 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const initialState = {
-    counter: 30
-}
+type CounterState = {
+  value: number;
+};
 
-export const counterSlice =  createSlice ({
-    name: "counter",
-    initialState,
-    reducers: {
-        increment: (state) => {
-            state.counter += 1
-        },
-        decrement: (state) => {
-            state.counter -= 1
-        }
-    }
-})
+const initialState: CounterState = {
+  value: 0,
+};
 
-export const { increment, decrement } = counterSlice.actions
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.value += action.payload;
+    },
+    decrementByAmount: (state, action: PayloadAction<number>) => {
+      state.value -= action.payload;
+    },
+    reset: (state) => {
+      state.value = 0;
+    },
+  },
+});
 
-export default counterSlice.reducer
+export const {
+  increment,
+  decrement,
+  incrementByAmount,
+  decrementByAmount,
+  reset,
+} = counterSlice.actions;
+
+export default counterSlice.reducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./features/counterSlice";
+import { userApi } from "./services/userApi";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
 
 export const store = configureStore({
-    reducer: {
-        counterReducer,
-    },
-})
+  reducer: {
+    counterReducer,
+    [userApi.reducerPath]: userApi.reducer,
+  },
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat([userApi.middleware]),
+});
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+setupListeners(store.dispatch);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
